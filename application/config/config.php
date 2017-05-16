9c1ba84c027b7cffbcaafd62b7aac7342bce30ec
nahidjawad <nahid.jawad@gmail.com>
2017-05-16 23:35:51 +1000

    Fixing base_url
    Added a function instead of hardcoding localhost. This code can be used
    in any machine.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/jb/';
+$config['base_url'] = 'http://'.getHostByName(getHostName()).'/jb/';
 
 /*
 |--------------------------------------------------------------------------
